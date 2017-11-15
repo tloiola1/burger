7cdee8191ace0b9b157da66dee213018ffba3c79
Tarciso <tarcisoloiola@hotmail.com>
2017-11-14 23:02:15 -0500

    Added DB Config

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,14 @@
 //Dependecies
-var password = require('../key.js');
-// console.log(password);
 // Create MySQL connection
 var mysql = require("mysql");
 var coloring = require('coloring');
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: password,
-  database: "burgers_db"
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "b6a1919e835123",
+  password: "2a3b3a52",
+  database: "heroku_7020a910d781eb0"
 });
 
 // Make connection.
@@ -24,3 +22,5 @@ connection.connect(function(err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+
+//mysql://b6a1919e835123:2a3b3a52@us-cdbr-iron-east-05.cleardb.net/heroku_7020a910d781eb0?reconnect=true
