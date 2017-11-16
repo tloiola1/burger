65b3e21a7b42b34d78e9ee77a6353b5c527000cf
Tarciso <tarcisoloiola@hotmail.com>
2017-11-15 22:40:24 -0500

    Add database

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,16 @@
 var mysql = require("mysql");
 var coloring = require('coloring');
 
-var pool = mysql.createPool({
-  connectionLimit: 100,
+var connection = mysql.createConnection({
+  port: 3306,
   host: "us-cdbr-iron-east-05.cleardb.net",
   user: "b6a1919e835123",
   password: "2a3b3a52",
-  database: "heroku_7020a910d781eb0",
-  debug: "false"
+  database: "heroku_7020a910d781eb0"
 });
 
 // Make connection.
-pool.connect(function(err) {
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -22,6 +21,6 @@ pool.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
-module.exports = pool;
+module.exports = connection;
 
 // mysql://b6a1919e835123:2a3b3a52@us-cdbr-iron-east-05.cleardb.net/heroku_7020a910d781eb0?reconnect=true
