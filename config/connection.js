96111ab3032df13fa44bcc5259f9d429354d4627
Tarciso <tarcisoloiola@hotmail.com>
2017-11-16 18:32:16 -0500

    add db

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,30 @@
-var mysql = require('mysql');
+// Set up MySQL connection.
+var mysql = require("mysql");
+var dotenv = require('dotenv').config();
 
-var connection = mysql.createConnection(process.env.JAWSDB_WHITE_URL);
+var connection;
 
-connection.connect();
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "Alessandro2015",
+        database: "burgers_db"
+    });
+}
 
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
