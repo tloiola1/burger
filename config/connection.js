0c4ed59cbeb3d3c9ce09838878cf741d069d373c
Tarciso <tarcisoloiola@hotmail.com>
2017-11-16 12:34:08 -0500

    Add database

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,15 @@ var coloring = require('coloring');
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "us-cdbr-iron-east-05.cleardb.net",
-  user: "b6a1919e835123",
-  password: "2a3b3a52",
-  database: "heroku_7020a910d781eb0"
+  host: "localhost",
+  user: "root",
+  password: "Alessandro2015",
+  database: "burgers_db"
+  // port: 3306,
+  // host: "us-cdbr-iron-east-05.cleardb.net",
+  // user: "b6a1919e835123",
+  // password: "2a3b3a52",
+  // database: "heroku_7020a910d781eb0"
 });
 
 // Make connection.
@@ -23,4 +28,4 @@ connection.connect(function(err) {
 // Export connection for our ORM to use.
 module.exports = connection;
 
-// mysql://b6a1919e835123:2a3b3a52@us-cdbr-iron-east-05.cleardb.net/heroku_7020a910d781eb0?reconnect=true
+//mysql://b6a1919e835123:2a3b3a52@us-cdbr-iron-east-05.cleardb.net/heroku_7020a910d781eb0?reconnect=true
