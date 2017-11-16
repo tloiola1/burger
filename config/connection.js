0d48b3c49cafa1d45da44baab769503aa3186620
Tarciso <tarcisoloiola@hotmail.com>
2017-11-16 18:18:55 -0500

    add db

--- config/connection.js
+++ config/connection.js
@@ -1,31 +1,6 @@
-//Dependecies
-// Create MySQL connection
-var mysql = require("mysql");
-var coloring = require('coloring');
+var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Alessandro2015",
-  database: "burgers_db"
-  // port: 3306,
-  // host: "us-cdbr-iron-east-05.cleardb.net",
-  // user: "b6a1919e835123",
-  // password: "2a3b3a52",
-  // database: "heroku_7020a910d781eb0"
-});
+var connection = mysql.createConnection(process.env.JAWSDB_WHITE_URL);
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("Connected ID:" + coloring.green(coloring.bold(connection.threadId)));
-});
+connection.connect();
 
-// Export connection for our ORM to use.
-module.exports = connection;
-
-//mysql://b6a1919e835123:2a3b3a52@us-cdbr-iron-east-05.cleardb.net/heroku_7020a910d781eb0?reconnect=true
