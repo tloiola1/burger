2b4b76f2f4aff719c9bac34412abc5674eeade6d
Tarciso <tarcisoloiola@hotmail.com>
2017-11-16 20:24:50 -0500

    Fixing issues

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ var mysql = require("mysql");
 
 var connection;
 
-if (process.env.JAWSDB_WHITE_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_WHITE_URL)
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 }
 else {
     connection = mysql.createConnection({
